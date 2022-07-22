fbef442c15763d69fb2aa8bc5e1270b15c39cb1a
jacarcelenb <carcelenjorge17@gmail.com>
2022-07-22 17:37:08 -0500

    Preparativos para el deploy

--- backend/backend_mongodb/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express');
-const router = Router();
-const { getCities, getStates } = require('../controllers/index')
-//API mongodb
-router.get('/cities',getCities);
-router.get('/states',getStates);
-
-module.exports = router;
